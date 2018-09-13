7c3dd6e9c57c6d500499c822da2d4ecc6c7c230a
Harunobu Tamaki <agunus-dei@hotmail.co.jp>
2018-09-13 14:16:44 +0900

    ビルド完了

--- Assets/Script/BulletScript.cs
+++ Assets/Script/BulletScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour {
     
     public static int bulletNumMax = 6; //弾数の最大数
-
+    public GameObject blood; //血液エフェクト
     SoundController sound;//音声コントローラ
 
 	// Use this for initialization
@@ -30,6 +30,7 @@ public class BulletScript : MonoBehaviour {
         //背景・敵・敵の弾に触れた場合、音を鳴らしてから消滅
         if(collision.gameObject.tag =="Stage" || collision.gameObject.tag =="Enemy" || collision.gameObject.tag == "EnemyBullet")
         {
+            Instantiate(blood, transform.position, transform.rotation);
             switch (collision.gameObject.tag)
             {
                 case "EnemyBullet":
